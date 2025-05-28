d6e3cc1f1afe80ad7ca3b447a89bfb90443fbfda
Danilo <danilo.schembri@gmail.com>
2025-05-28 18:55:22 +0200

    Added license banner

--- bin/restic_backup.ps1
+++ bin/restic_backup.ps1
@@ -1,4 +1,20 @@
-. "$PSScriptRoot\restic_variables.ps1"
+# restic-windows-easy-scripts/
+# Copyright (C) 2025 Danilo Schembri
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+ . "$PSScriptRoot\restic_variables.ps1"
 
 foreach ($RESTIC_DIR_SOURCE in $RESTIC_BACKUP_DIRECTORIES_SOURCE) {
 	& $RESTIC_EXE backup "$RESTIC_DIR_SOURCE" --verbose -r $RESTIC_REPOSITORY --password-command $RESTIC_PASSWORD_COMMAND $args
